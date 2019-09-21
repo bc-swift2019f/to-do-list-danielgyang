589169191bb381bc984082df7bd59320954b8c3b
Daniel Yang <yangafc@bc.edu>
2019-09-20 20:27:50 -0400

    5.1 completed

---
+++ To Do List/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  To Do List
+//
+//  Created by Daniel Yang on 9/20/19.
+//  Copyright © 2019 Daniel Yang. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var toDoField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
